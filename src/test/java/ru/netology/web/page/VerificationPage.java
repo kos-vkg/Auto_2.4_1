1807d4f11b5cdbb4b95db276009cd5cccd2f74bd
kos-vkg <kvertenov@mail.ru>
2021-03-05 13:51:10 +0300

    -corr.p3

--- src/test/java/ru/netology/web/page/VerificationPage.java
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -9,14 +9,17 @@ import static com.codeborne.selenide.Selenide.$;
 public class VerificationPage {
     private SelenideElement codeField = $("[data-test-id=code] input");
     private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private SelenideElement heading = $("[data-test-id=dashboard]");
 
     public VerificationPage() {
         codeField.shouldBe(visible);
     }
 
-    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+    public void validVerify(DataHelper.VerificationCode verificationCode) {
         codeField.setValue(verificationCode.getCode());
         verifyButton.click();
-        return new DashboardPage();
+        heading.shouldBe(visible);
+        return;
     }
+
 }
